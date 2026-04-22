c9064ee2e71bc00b5e761dfc450cd3928463375f
reymahess <reynatamahessa@gmail.com>
2026-04-22 11:58:34 +0700

    add nama and set harga/stok

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 //package berbeda, bukan subclass class main
 
-import base.Produk;
-
 public class Main {
 
     public static void main(String[] args) {
         Produk p = new Produk("Indomie Goreng", 3500, 100, "Makanan");
         
 
-        System.out.println(p.stok);
-        System.out.println(p.harga);
-        System.out.println(p.kategori);
-        System.out.println(p.nama);
-    
+        System.out.println(p.getNama());
+        System.out.println(p.getHarga());
+        //System.out.println(p.harga);
+        //System.out.println(p.kategori);
+        //System.out.println(p.nama);
+        p.setHarga(20000.0);
+        System.out.println(p.getHarga());
+        p.setStok(50);
+        System.out.println(p.getStok());
+
+        p.tampilInfo();
     }
     
 }
