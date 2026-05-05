3b42e62b1a907a3a051f5f24fc66a22e2d60f670
reymahess <reynatamahessa@gmail.com>
2026-05-05 10:28:07 +0700

    Create Main

--- Main.java
+++ Main.java
@@ -3,20 +3,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        Produk p = new Produk("Indomie Goreng", 3500, 100, "Makanan");
-        
-
-        System.out.println(p.getNama());
-        System.out.println(p.getHarga());
-        //System.out.println(p.harga);
-        //System.out.println(p.kategori);
-        //System.out.println(p.nama);
-        p.setHarga(20000.0);
-        System.out.println(p.getHarga());
-        p.setStok(50);
-        System.out.println(p.getStok());
-
-        p.tampilInfo();
+        elektronik MesinCuci = new elektronik("Samsung", 2000000.0, 20, "Alat Rumah", 5);
+        MesinCuci.setGaransi(0);
+        MesinCuci.tampilData();
     }
-    
 }
