7cc22d1d22e8a392b6444e3a2a2c2cafb7f690c4
reymahess <reynatamahessa@gmail.com>
2026-05-06 10:31:36 +0700

    elektronik, makanan

--- Main.java
+++ Main.java
@@ -3,7 +3,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        elektronik MesinCuci = new elektronik("Samsung", 2000000.0, 20, "Alat Rumah", 5);
+        elektronik laptop = new elektronik("Laptop Asus", "Asus", 8500000, 10, 2);
+        elektronik hp = new elektronik("Handphone Samsung", "Samsung", 3000000, 25, 1);
+        makanan indomie = new makanan("Indomie Goreng", 3500, 100, "Makanan Berat", "2024-12-31");
+        makanan roti = new makanan("Roti Tawar", 12000, 100, "Makanan Ringan", "2024-11-30");
+
+
+
+
+
+
+
         MesinCuci.setGaransi(0);
         MesinCuci.tampilData();
     }
